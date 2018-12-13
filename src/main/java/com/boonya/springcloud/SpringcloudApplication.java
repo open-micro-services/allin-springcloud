aef7fef8909b6e833dd40fb4a86716d8c95ad397
boonya <boonya@sina.com>
2018-12-13 18:57:47 +0800

    build a base archetecture

---
+++ src/main/java/com/boonya/springcloud/SpringcloudApplication.java
@@ -0,0 +1,16 @@
+package com.boonya.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/boonya/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.boonya.springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
