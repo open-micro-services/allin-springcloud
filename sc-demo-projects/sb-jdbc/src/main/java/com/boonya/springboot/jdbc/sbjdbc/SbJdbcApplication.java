a7e1259104310ba9e9285d7e29f41407ee5bf835
boonyachengdu <boonyachengdu@gmail.com>
2019-05-24 11:56:29 +0800

    添加sc-demo-projects作为快速测试的样例

---
+++ sc-demo-projects/sb-jdbc/src/main/java/com/boonya/springboot/jdbc/sbjdbc/SbJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.boonya.springboot.jdbc.sbjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJdbcApplication.class, args);
+    }
+
+}
---
+++ sc-demo-projects/sb-jdbc/src/main/java/com/boonya/springboot/jdbc/sbjdbc/mysql/JdbcTemplateController.java
@@ -0,0 +1,25 @@
+package com.boonya.springboot.jdbc.sbjdbc.mysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
+@RestController
+@RequestMapping("/jdbc")
+public class JdbcTemplateController{
+
+    @Autowired(required=true)
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Map<String, Object>> query(){
+        System.out.println(""+jdbcTemplate.getDataSource());
+        List<Map<String, Object>> list=jdbcTemplate.queryForList("select * from xht_events order by xiancode limit 0,10");
+        return list;
+    }
+}
