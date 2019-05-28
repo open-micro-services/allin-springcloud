b674de0fadbc749c63fbb825af7a3aff3f8ae11d
boonyachengdu <boonyachengdu@gmail.com>
2019-05-28 14:58:36 +0800

    sb-websocket项目WebSocketServer多线程安全性修改

---
+++ sc-demo-projects/sb-websocket/src/main/java/com/boonya/springboot/websocket/sbwebsocket/server/WebSocketMessage.java
@@ -0,0 +1,47 @@
+package com.boonya.springboot.websocket.sbwebsocket.server;
+
+import java.io.Serializable;
+
+/**
+ * WebSocket消息类
+ */
+public class WebSocketMessage implements Serializable {
+
+    public int status=0;
+
+    private String message="";
+
+    private Object data=new Object();
+
+    public WebSocketMessage(int status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        if(data!=null){
+            this.data = data;
+        }
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
--- sc-demo-projects/sb-websocket/src/main/java/com/boonya/springboot/websocket/sbwebsocket/server/WebSocketServer.java
+++ sc-demo-projects/sb-websocket/src/main/java/com/boonya/springboot/websocket/sbwebsocket/server/WebSocketServer.java
@@ -1,5 +1,6 @@
 package com.boonya.springboot.websocket.sbwebsocket.server;
 
+import com.boonya.springboot.websocket.sbwebsocket.util.JSONUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -7,36 +8,49 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * WS服务端
+ */
 @ServerEndpoint("/websocket/{uuid}")
 @Component
 public class WebSocketServer {
 
     private final static Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
 
-    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
-    private static int onlineCount = 0;
+    // 静态变量，用来记录当前在线连接数,应该把它设计成线程安全的，原子变量操作时最合适
+    private static AtomicInteger onlineCount =new AtomicInteger(0);
 
-    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    //concurrent包的线程安全ConcurrentHashMap，用来存放每个客户端对应的WebSocketServer对象
     private static ConcurrentHashMap<String ,WebSocketServer> webSocketSet = new ConcurrentHashMap<String,WebSocketServer>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
-    //接收uuid
+    //与某个客户端连接标识uuid
     private String uuid="";
     /**
      * 连接建立成功调用的方法*/
     @OnOpen
     public void onOpen(Session session,@PathParam("uuid") String uuid) {
         this.session = session;
-        webSocketSet.put(uuid,this);     //加入set中
-        addOnlineCount();           //在线数加1
-        logger.info("有新窗口开始监听:"+uuid+",当前在线人数为" + getOnlineCount());
         this.uuid=uuid;
+        // 记录连接客户端标识
+        webSocketSet.put(uuid,this);
+        // 增加在线数量(递增)
+        WebSocketServer.incrementOnlineCount();
+        logger.info("有新窗口开始监听:"+uuid+",当前在线人数为" + WebSocketServer.getOnlineCount());
         try {
-            sendMessage("连接成功");
+            Map<String,Object> map=new HashMap<String,Object>();
+            map.put("id",UUID.randomUUID().toString());
+            map.put("message","连接成功消息");
+            String json= JSONUtil.getJsonFromObject(new WebSocketMessage(1,"连接成功",map));
+            sendMessage(json);
         } catch (IOException e) {
             logger.error("websocket IO异常");
         }
@@ -47,28 +61,35 @@ public class WebSocketServer {
      */
     @OnClose
     public void onClose() {
-        webSocketSet.remove(this.uuid);  //从set中删除
-        subOnlineCount();           //在线数减1
+        //从内存对象中移除连接标识
+        webSocketSet.remove(this.uuid);
+        // 递减连接数量
+        WebSocketServer.decrementOnlineCount();
         logger.info("有一连接关闭！当前在线人数为" + getOnlineCount());
     }
 
     /**
      * 收到客户端消息后调用的方法
      *
-     * @param message 客户端发送过来的消息*/
+     * @param message 客户端发送过来的消息
+     */
     @OnMessage
     public void onMessage(String message, Session session) {
         logger.info("收到来自窗口"+uuid+"的信息:"+message);
         try{
             // 收到消息指令应该对应做处理，然后发送响应消息给客户端
-            broadcastOne("服务端已收到页面发送的消息："+message,this.uuid);
-
+            Map<String,Object> map=new HashMap<String,Object>();
+            map.put("id",UUID.randomUUID().toString());
+            map.put("message",message);
+            String json= JSONUtil.getJsonFromObject(new WebSocketMessage(1,"接收消息成功",map));
+            WebSocketServer.broadcastOne(json,this.uuid);
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
     /**
+     * 服务端产生错误调用方法
      *
      * @param session
      * @param error
@@ -79,13 +100,13 @@ public class WebSocketServer {
         error.printStackTrace();
     }
     /**
-     * 实现服务器主动推送
+     * 实现服务器主动推送消息
      */
     public void sendMessage(String message) throws IOException {
         this.session.getBasicRemote().sendText(message);
     }
     /**
-     * 单播
+     * 单播消息
      * */
     public static void broadcastOne(String message,@PathParam("uuid") String uuid) throws IOException {
         logger.info("推送消息到窗口"+uuid+"，推送内容:"+message);
@@ -93,6 +114,7 @@ public class WebSocketServer {
             try {
                 if(item.uuid.equals(uuid)){
                     item.sendMessage(message);
+                    logger.info("UUID:"+item.uuid+"....已推送!");
                 }
             } catch (IOException e) {
                 continue;
@@ -101,7 +123,7 @@ public class WebSocketServer {
     }
 
     /**
-     * 广播
+     * 广播消息
      * */
     public static void broadcastAll(String message) throws IOException {
         logger.info("推送消息到所有打开窗口，推送内容:"+message);
@@ -109,22 +131,33 @@ public class WebSocketServer {
         for (WebSocketServer item : webSocketSet.values()) {
             try {
                 item.sendMessage(message);
+                logger.info("UUID:"+item.uuid+"....已推送!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * 获取在线数量
+     * @return
+     */
     public static synchronized int getOnlineCount() {
-        return onlineCount;
+        return onlineCount.get();
     }
 
-    public static synchronized void addOnlineCount() {
-        WebSocketServer.onlineCount++;
+    /**
+     * 递增在线数量
+     */
+    public static synchronized void incrementOnlineCount() {
+        WebSocketServer.onlineCount.incrementAndGet();
     }
 
-    public static synchronized void subOnlineCount() {
-        WebSocketServer.onlineCount--;
+    /**
+     * 递减在线数量
+     */
+    public static synchronized void decrementOnlineCount() {
+        WebSocketServer.onlineCount.decrementAndGet();
     }
 
 }
