378ed3e61f0bf9296ca78043aa22a04b5e7ee3a5
boonyachengdu <boonyachengdu@gmail.com>
2019-05-30 15:21:37 +0800

    解决多环境配置启动改变后WebSocket的访问地址问题

---
+++ sc-demo-projects/dp-websocket/src/main/java/com/dataservice/websocket/dpwebsocket/config/ServerConfig.java
@@ -0,0 +1,28 @@
+package com.dataservice.websocket.dpwebsocket.config;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+@Component
+public class ServerConfig  implements ApplicationListener<WebServerInitializedEvent> {
+
+    private int serverPort;
+
+    public String getBaseUrl() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "http://"+address.getHostAddress() +":"+this.serverPort;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.serverPort = event.getWebServer().getPort();
+    }
+
+}
