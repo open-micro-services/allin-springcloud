c78bddea440a222f55832979a79793c86b1ade8e
boonyachengdu <boonyachengdu@gmail.com>
2019-06-03 11:03:31 +0800

    添加Redis事务测试

--- sc-demo-projects/dp-websocket/src/main/java/com/dataservice/websocket/dpwebsocket/controller/RedisController.java
+++ sc-demo-projects/dp-websocket/src/main/java/com/dataservice/websocket/dpwebsocket/controller/RedisController.java
@@ -4,6 +4,7 @@ import com.dataservice.websocket.dpwebsocket.redis.RedisChannel;
 import com.dataservice.websocket.dpwebsocket.redis.RedisService;
 import com.dataservice.websocket.dpwebsocket.redis.mq.MQProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,10 @@ public class RedisController {
 
     @Autowired
     RedisService redisService;
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
 
     /**
      * 发布订阅消息
@@ -64,6 +69,28 @@ public class RedisController {
         // 将消息放入队列
         //MQProducer.produce(RedisChannel.TEST_CHANNEL.getValue(),message);
         redisService.produce(RedisChannel.TEST_CHANNEL.getValue(),message);
+        return "success";
+    }
+
+    @RequestMapping("/redis/transaction/userCount")
+    @ResponseBody
+    public String redisTransaction(int value){
+        String key="user_count";
+        try{
+            // 开启事务支持
+            redisTemplate.setEnableTransactionSupport(true);
+            // 监视数据key
+            redisTemplate.watch(key);
+            // 开启事务
+            redisTemplate.multi();
+            // 预处理执行语句
+            redisTemplate.opsForSet().add(key,value);
+            // 执行事务处理
+            redisTemplate.exec();
+        }catch(Exception e){
+            e.printStackTrace();
+            return "failed";
+        }
         return "success";
     }
 }
