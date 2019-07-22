3032c809480498340409bfebb6aed3454afccd18
boonyachengdu <boonyachengdu@gmail.com>
2019-07-22 18:23:41 +0800

    修改测试说明

---
+++ sc-demo-projects/sb-sqlite/src/main/java/com/boonya/sbsqlite/Model/ReqBody.java
@@ -0,0 +1,30 @@
+package com.boonya.sbsqlite.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ReqBody {
+
+    private String Name;
+
+    private String Email;
+
+    public String getName() {
+        return Name;
+    }
+    @JsonProperty(value="Name")
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+    @JsonProperty(value="Email")
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+
+
+
+}
---
+++ sc-demo-projects/sb-sqlite/src/main/java/com/boonya/sbsqlite/SqliteApplication.java
@@ -0,0 +1,12 @@
+package com.boonya.sbsqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqliteApplication.class, args);
+    }
+}
