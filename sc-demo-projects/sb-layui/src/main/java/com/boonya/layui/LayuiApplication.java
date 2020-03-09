6969c1465fd0ba7e7e9255e1f89c9ea42b3475cd
boonyachengdu <boonyachengdu@gmail.com>
2020-03-09 23:23:20 +0800

    Layui测试

---
+++ sc-demo-projects/sb-layui/src/main/java/com/boonya/layui/LayuiApplication.java
@@ -0,0 +1,13 @@
+package com.boonya.layui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayuiApplication.class, args);
+    }
+
+}
