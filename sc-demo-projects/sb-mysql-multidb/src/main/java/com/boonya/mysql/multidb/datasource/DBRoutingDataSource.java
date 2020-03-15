6868320796b33b903f98bda144946a5932969892
boonyachengdu <boonyachengdu@gmail.com>
2020-03-15 12:55:13 +0800

    数据源路由备注修改

--- sc-demo-projects/sb-mysql-multidb/src/main/java/com/boonya/mysql/multidb/datasource/DBRoutingDataSource.java
+++ sc-demo-projects/sb-mysql-multidb/src/main/java/com/boonya/mysql/multidb/datasource/DBRoutingDataSource.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
  * @ClassName: DBRoutingDataSource
- * @Description: TODO(功能说明:Spring源码提供的数据库连接池路由功能)
+ * @Description: TODO(功能说明:Spring源码提供的数据源路由功能)
  * @author: pengjunlin
  * @motto: 学习需要毅力，那就秀毅力
  * @date 2020/3/15 0:26
