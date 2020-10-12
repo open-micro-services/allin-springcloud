4705f3a3d8d14cdb3a834169d34ed51f732fc201
boonyachengdu <boonya@sina.com>
2020-10-12 18:16:02 +0800

    修改Eureka项目服务发现注册问题

--- sc-eureka-client/src/test/java/com/boonya/springcloud/eureka/client/EurekaClientApplicationTests.java
+++ sc-eureka-client/src/test/java/com/boonya/springcloud/eureka/client/EurekaClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class EurekaClientApplicationTests {
 
     @Test
     public void contextLoads() {
--- sc-eureka-server/src/test/java/com/boonya/springcloud/eureka/server/EurekaServerApplicationTests.java
+++ sc-eureka-server/src/test/java/com/boonya/springcloud/eureka/server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.boonya.springcloud.eureka.client;
+package com.boonya.springcloud.eureka.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class EurekaServerApplicationTests {
 
     @Test
     public void contextLoads() {
