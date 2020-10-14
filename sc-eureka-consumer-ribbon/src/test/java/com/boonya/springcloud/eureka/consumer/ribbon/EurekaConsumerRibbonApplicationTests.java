b9ffa66dcd2d1371cc594cc5c4adb96e140ce4b9
boonyachengdu <boonya@sina.com>
2020-10-14 14:59:05 +0800

    Eureka结合Ribbon实现服务负载均衡调用

---
+++ sc-eureka-consumer-ribbon/src/test/java/com/boonya/springcloud/eureka/consumer/ribbon/EurekaConsumerRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boonya.springcloud.eureka.consumer.ribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
